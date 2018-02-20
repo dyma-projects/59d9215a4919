d283e686e32ad000f19d2b84c2ff3760bf8a02b2
Yossi COHEN <ycohen@elcimai.com>
2018-02-20 09:48:36 +0100

    add attribut status in directive component

--- src/app/directive/directive.component.ts
+++ src/app/directive/directive.component.ts
@@ -3,17 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directive',
   templateUrl: './directive.component.html',
-  styleUrls: ['./directive.component.css']
+  styleUrls: ['./directive.component.css'],
+
 })
 export class DirectiveComponent implements OnInit {
-  public altImage:string;
-  public srcImage:string;
+  public status: boolean = true;
 
   constructor() { }
   
   ngOnInit() {
-    this.srcImage = "https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg";
-    this.altImage = "paysage";
+   
   }
 
 }
